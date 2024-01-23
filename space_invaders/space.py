01fcbd9034dc75fe91b0ae2ea62a0a8198001cd1
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2024-01-23 20:15:41 +0600

    Add spaceship bullet and alien collision - bullets not killed

--- space_invaders/space.py
+++ space_invaders/space.py
@@ -70,6 +70,8 @@ class Bullets(pygame.sprite.Sprite):
         self.rect.y -= 5
         if self.rect.bottom < 0:
             self.kill()
+        if pygame.sprite.spritecollide(self, alien_group, True):
+            pass
 
 class Aliens(pygame.sprite.Sprite):
     def __init__(self, x, y):
