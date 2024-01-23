ed7cb4580e4f11c824a27b79ccdcfbb08e17ba29
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2024-01-23 17:31:23 +0600

    Example shapes

--- pong.py
+++ pong.py
@@ -3,6 +3,10 @@ import pygame
 # Initialize the modules in pygame
 pygame.init()
 
+# Define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
@@ -19,6 +23,10 @@ while not done:
     # Game logic
 
     # Drawing code
-    screen.fill((0, 0, 0))
+    screen.fill(BLACK)
+    # Some example shapes
+    pygame.draw.rect(screen, WHITE, [50, 50, 20, 20])
+    pygame.draw.line(screen, WHITE, [100, 100], [150, 150], 5)
+    pygame.draw.circle(screen, WHITE, [60, 250], 40)
 
     pygame.display.update()
